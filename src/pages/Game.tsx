d8477f20e08c5d4e69c3a5adc7407e0cf285bfb3
Tanuj Sharma <hellotanujsharma@gmail.com>
2026-02-04 22:06:40 +0530

    add: buttons

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,16 @@
 // import { Button } from "@/components/ui/button";
 
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import {
+  ArrowBigLeft,
+  ArrowBigRight,
+  ArrowBigUp,
+  ArrowBigDown,
+} from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
-const TICK_SPEED = 200;
+const TICK_SPEED = 150;
 const DISPLAY_SIZE = 25;
 const DISPLAY_GRID: string[][] = Array.from({ length: DISPLAY_SIZE }, () => {
   return new Array(DISPLAY_SIZE).fill("");
@@ -24,6 +31,11 @@ const Game = () => {
   const [appleState, setAppleState] = useState<number[]>([3, 3]);
   const appleRef = useRef<number[] | null>([3, 3]);
 
+  const upBtnRef = useRef(null);
+  const leftBtnRef = useRef(null);
+  const rightBtnRef = useRef(null);
+  const bottomBtnRef = useRef(null);
+
   /* ------------------------------- HELPER FUNC ------------------------------- */
   const isSnakeBody = (x: number, y: number) => {
     return snakeBody.some(([a, b]) => a === x && b === y);
@@ -43,8 +55,17 @@ const Game = () => {
 
   /* ------------------------------- UPDATE LOOP ------------------------------- */
   useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      switch (e.code) {
+    if (
+      !upBtnRef.current ||
+      !bottomBtnRef.current ||
+      !leftBtnRef.current ||
+      !rightBtnRef.current
+    )
+      return;
+
+    const handleKeyDown = (e?: KeyboardEvent | null, btnPressed?: string) => {
+      const moveTo = e?.code || btnPressed;
+      switch (moveTo) {
         case "ArrowRight":
           if (movementDir.current === "left") break;
           changePos.x = 1;
@@ -78,8 +99,6 @@ const Game = () => {
     const generateApple = () => {
       const randomX = Math.floor(Math.random() * DISPLAY_SIZE);
       const randomY = Math.floor(Math.random() * DISPLAY_SIZE);
-
-      console.log(randomX, randomY);
 
       appleRef.current = [randomX, randomY];
       setAppleState([randomX, randomY]);
@@ -132,6 +151,19 @@ const Game = () => {
 
     document.addEventListener("keydown", handleKeyDown);
 
+    upBtnRef.current.addEventListener("click", () =>
+      handleKeyDown(null, "ArrowUp"),
+    );
+    bottomBtnRef.current.addEventListener("click", () =>
+      handleKeyDown(null, "ArrowDown"),
+    );
+    rightBtnRef.current.addEventListener("click", () =>
+      handleKeyDown(null, "ArrowRight"),
+    );
+    leftBtnRef.current.addEventListener("click", () =>
+      handleKeyDown(null, "ArrowLeft"),
+    );
+
     return () => {
       clearInterval(clock);
       document.removeEventListener("keydown", handleKeyDown);
@@ -139,11 +171,13 @@ const Game = () => {
   }, [appleRef, movementDir]);
 
   return (
-    <div className="w-full h-screen bg-background grid place-content-center gap-5">
+    <div className="w-full h-screen bg-background flex flex-col items-center pt-10 gap-5">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         <span className="opacity-60">Score: </span> {score}
       </h1>
-      <div className={`w-100 h-100 border-2 grid grid-rows-25 grid-cols-25`}>
+      <div
+        className={`w-80 md:w-90 lg:w-100 aspect-square border-2 grid grid-rows-25 grid-cols-25`}
+      >
         {DISPLAY_GRID.map((rows, y) => {
           return rows.map((cell, x) => {
             return (
@@ -161,6 +195,42 @@ const Game = () => {
           });
         })}
       </div>
+      <div className="grid grid-cols-3 grid-rows-3 w-60 h-50">
+        <Button
+          className="col-start-2 h-full"
+          variant={"secondary"}
+          ref={upBtnRef}
+        >
+          <ArrowBigUp />
+        </Button>
+        <Button
+          className="row-start-2 h-full"
+          variant={"secondary"}
+          ref={leftBtnRef}
+        >
+          <ArrowBigLeft />
+        </Button>
+        <Button
+          className="row-start-2 col-start-3 h-full"
+          variant={"secondary"}
+          ref={rightBtnRef}
+        >
+          <ArrowBigRight />
+        </Button>
+        <Button
+          className="row-start-3 col-start-2 h-full"
+          variant={"secondary"}
+          ref={bottomBtnRef}
+        >
+          <ArrowBigDown />
+        </Button>
+      </div>
+      <p className="opacity-60">
+        Developed by{" "}
+        <a href="https:github.com/tanujsharma911" className="underline">
+          Tanuj Sharma
+        </a>
+      </p>
     </div>
   );
 };
